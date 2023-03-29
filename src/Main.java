6ad5ca87372d3296ed347407948a122d055a40da
Zergi4 <ksyssau@ya.ru>
2023-03-29 20:20:29 +0300

    1 задача

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //hw 5.1
+        byte clientOs = 1;
+        switch (clientOs) {
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке.");
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке.");
+        }
+        //
     }
 }
