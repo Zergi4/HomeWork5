c8de3911f03245882d1de20186ad516827986430
Zergi4 <ksyssau@ya.ru>
2023-03-29 21:52:24 +0300

    3 задача

--- src/Main.java
+++ src/Main.java
@@ -17,12 +17,23 @@ public class Main {
             } else {
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке");
             }
-            } else if (clientOs2 == 1) {
-                System.out.println("Установите версию приложения для Android по ссылке.");
-            } else {
-                System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else if (clientOs2 == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
 
+        }
+        //hw5.3
+        int year = 2000;
+        if (year % 4 == 0) {
+            if ((year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println("Год " + year + " является высокосным.");
+            } else {
+                System.out.println("Год " + year + " не является высокосным.");
             }
+        } else {
+            System.out.println("Год " + year + " не является высокосным.");
         }
 
     }
+}
